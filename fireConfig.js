f538ebb742f54eaf46cf326b0be200b95734395f
Bergietibay <126254514+Bergietibay@users.noreply.github.com>
2023-05-02 19:50:08 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdNbiBbwrMKe1-K_KM_1_qm46TqafA17w",
+    authDomain: "crudfire-f243c.firebaseapp.com",
+    projectId: "crudfire-f243c",
+    storageBucket: "crudfire-f243c.appspot.com",
+    messagingSenderId: "561461960897",
+    appId: "1:561461960897:web:bbbbb6297ca787ca5d3440",
+    measurementId: "G-HY6EZV5FLM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
